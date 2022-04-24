98e26ab4297076cc8f1e3ff174d303a240f90646
joshua1nine <59374346+joshua1nine@users.noreply.github.com>
2022-04-24 14:16:50 -0500

    first commit

---
+++ web/tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				brand: '#0C0D3B',
+			},
+			fontFamily: {
+				sans: [
+					'Open Sans',
+					'ui-sans-serif',
+					'system-ui',
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'Segoe UI',
+					'Roboto',
+					'Helvetica Neue',
+					'Arial',
+					'Noto Sans',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+					'Noto Color Emoji',
+				],
+			},
+		},
+	},
+	plugins: [],
+};
